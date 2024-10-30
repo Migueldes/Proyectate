05a064a871fabe9c9a0d30e703d3483359d3aeef
adrianvera47 <adrianvera417@gmail.com>
2024-10-29 22:16:28 -0600

    funcion a priori y fase de entrenamiento

--- backend/modelo/modeloIA.js
+++ backend/modelo/modeloIA.js
@@ -1,7 +1,7 @@
 //Codigo de la ia
 // Datos globales
-var etiqueta=str=""
-var salida_x1=str=""
+let etiqueta="";
+let salida_x1="";
 
 // Se esta usando el modulo de csv-parse para leer el archivo
 const { readFileSync } = require('fs');
@@ -51,27 +51,48 @@ function probabilidadesProc() {
 const paso1res = probabilidadesProc();
 console.log(paso1res); // Pa ver que sale
 
-
 // Paso 2.- sacar las evidencias a priori
 // PRIMER COLUMNA DE LA TABLA, se lee,
-function Probabilidad_priori() {
-  let cinestesico_a_x1 = int=0
-  let cinestesico_b_x1 = int=0
-  let cinestesico_c_x1 = int=0
-  let visual_a_x1 = int=0
-  let visual_b_x1 = int=0
-  let visual_c_x1 = int=0
-  let auditivo_a_x1 = int=0
-  let auditivo_b_x1 = int=0
-  let auditivo_c_x1 = int=0
+  function Probabilidad_priori() {
+    // Se crea un diccionario para hacer todas las combinaciones de cada tabla
+    let tablas_dicc = {};
+    const etiquetas = ["cinestecico", "visual", "auditivo"];
+    const salidas = ["a", "b", "c"];
+  
+    // For anidados para crear las 20 tablas y sus combinaciones de elementos
+    for (let i = 0; i < 20; i++) {
+      for (let etiqueta of etiquetas) {
+        for (let salida of salidas) {
+          tablas_dicc[`${etiqueta},${salida},x${i + 1}`] = 0;
+        }
+      }
+    }
+    // Recorremos los datos en csvContenido y actualizamos tablas_dicc
+    for (let j = 0; j < csvContenido.length; j++) {
+      const etiqueta = csvContenido[j]["etiqueta"];
+      
+      // itera sobre las 20 tablas para meter los datos a su respectivo campo
+      for (let k = 0; k < 20; k++) {
+          const salida_x1 = csvContenido[j][`x${k + 1}`];
 
+          // Generamos clave ya que no se pueden crear claves compuestas como en python
+          const clave = `${etiqueta},${salida_x1},x${k + 1}`;
 
-// ya creadas las variables de la tabla entonces
-// creamos el for que va a recorrer todo
-for (let i = 0; i < csvContenido.length; i++ ) {
-  const etiqueta = csvContenido[i].etiqueta[i] 
+          // Verificamos si la clave existe y, si es así, incrementamos el valor
+          if (clave in tablas_dicc) {
+              tablas_dicc[clave] += 1;
+          }
+      }
+  }
+    return tablas_dicc;
 }
-}
+
+const paso2res = Probabilidad_priori(paso1res);
+console.log(paso2res);
+
+  // ya creadas las variables de la tabla entonces
+  // creamos el for que va a recorrer todo
+
 
 //Cambiar form data por el dato para la prediccion
 //const procesoIA = async (formData) => {
